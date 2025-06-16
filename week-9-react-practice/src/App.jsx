a37516ce90ed82525fb0da0bd4de8935b39b32aa
AryanMahajan <mahajanaryan0409@gmail.com>
2025-06-17 00:10:38 +0530

    dependancy-array

--- week-9-react-practice/src/App.jsx
+++ week-9-react-practice/src/App.jsx
@@ -1,58 +1,49 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react"
 
+// Re-learning cleanups, useeffect, and learning about dependancy array
 function App() {
 
-  let [counterVisible, setCounterVisible] = useState(true)
-
-  useEffect(()=>{
-    setInterval(()=>{
-      setCounterVisible(c => !c)
-    },5000)
-  },[])
-
-  return (
-    <div>
-      hi
-      { counterVisible && <Counter />}
-      hello
-      {/* <Counter></Counter> */}
-    </div>
-  )
-}
-
-
-function Counter() {
   const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
 
-  // console.log("counter");
+  function increase() {
+    setCount(c => c+1)
+  }
 
-  //hooking into the lifecycle events
-  useEffect(function () {
-    console.log("on mount")
-    let clock = setInterval(() => {
-      // setCount(count => count+1)
-      console.log("inside the setInterval");
-      setCount((count) => {
-        return count+1
-      });
-    }, 1000)
-
-    return function() {
-      console.log("unmount");
-      
-      clearInterval(clock)
-    }
-
-  }, []); //dependancy array
-
-  function increaseCount() {
-    setCount(count + 1)
+  function decrease() {
+    setCount2(c => c-1)
   }
 
   return <div>
-    <h1 id="text">{count}</h1>
-    <button onClick={increaseCount}>Increase</button>
+    <Counter count = { count } count2 = {count2}/>
+    <button onClick={increase}>Increase Count</button>
+    <button onClick={decrease}>Decrease Count</button>
   </div>
 }
+
+// mounting, re-rendring, unmounting
+function Counter(props) {
+
+  useEffect(() =>{
+    console.log("mount")
+
+    return () => {
+      console.log("unmount")
+    }
+  },[]);
+
+  useEffect(()=>{
+    console.log("count has changed")
+  },[props.count])
+
+  return <div>
+    Counter1 {props.count}
+    <br /> 
+    Counter2 {props.count2}
+  </div>
+}
+
+
+
 
 export default App
